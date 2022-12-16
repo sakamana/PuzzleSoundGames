62258877c4daa858ff199f24b621fc1c314b0958
saba <56290485+sakamana@users.noreply.github.com>
2022-12-16 14:27:18 +0900

    truecount戻した
    スコアに何点加算されているかわからなかったので

--- PuzzleSound/Assets/SampleCode/Board.cs
+++ PuzzleSound/Assets/SampleCode/Board.cs
@@ -364,7 +364,7 @@ public class Board : MonoBehaviour {
         {
             longd = (int)targetPiece.GetLength();
             touchnow = true;
-            Debug.Log(longd);
+            //Debug.Log(longd);
         }
         if(touchnow)
         {
@@ -398,7 +398,7 @@ public class Board : MonoBehaviour {
         var nlong = PieceKind.LongNote;
         var nmusic = PieceKind.MusicNote;
 
-        //int truecount = 0;
+        int truecount = 0;
         // 削除フラグが立っているノーツを削除する
         foreach (var piece in board)
         {
@@ -408,7 +408,8 @@ public class Board : MonoBehaviour {
                 if(piece.musicFlag)
                 {
                     GameManager.score++;
-                    //Debug.Log(truecount);
+                    truecount++;
+                    Debug.Log(truecount);
                 }
                 Destroy(piece.gameObject);
             }
